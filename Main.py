bd6da5dacf3fbb2d1898905425083c9c59b6529e
hamed <69624616+HamedGolabi@users.noreply.github.com>
2024-06-01 14:08:36 +0330

    Update Main.py

--- Main.py
+++ Main.py
@@ -3,7 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def hello_hamed():
+    return render-template('home.html') 
 if __name__ == '__main__' :
   app.run(host='0.0.0.0',debug=true) 
